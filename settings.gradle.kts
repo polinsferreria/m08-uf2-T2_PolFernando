896c328b345b258165e59af6e00405435efaa188
Nando1702 <120483561+Nando1702@users.noreply.github.com>
2024-03-01 15:38:59 +0100

    primer comit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DAM2M08UF2T2RADIO"
+include(":app")
+ 
